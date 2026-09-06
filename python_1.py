fb9b64e90acc1ca8e7c6d0ca1e0f2e45fbf6f5f8
SCODEX-ARCH <ranjanaditya937@gmail.com>
2026-09-06 17:38:45 +0530

    All i learned through python on day 1
    This file contains examples of various Python operators, type conversions, and input handling.

---
+++ python_1.py
@@ -0,0 +1,132 @@
+arithmetic operators 
+# a=5
+#  b=2 
+
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+# print(a%b)
+# print(a**b)
+
+# relational operators
+# a=50
+# b=20
+
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a>b)
+# print(a<b)
+
+# assignment operators 
+
+# num=10
+# num-=10
+# print("num:",num)
+
+# num=10
+# num*=10
+# print("num:",num)
+
+# num=10
+# num/=5
+# print("num:",num)
+
+# num=10
+# num%=10
+# print("num:",num)
+
+# num=10
+# num**=10
+# print("num:",num)
+
+# logical operators
+
+# a=50
+# b=30
+
+# print(not False)
+# print(not (a>b))
+
+# val1 = False
+# val2 = False
+# print("ANd operator:", val1 and val2)
+# print("OR operator", (a==b) or (a>b))
+
+# type conversion
+
+# a=2
+# b=4.24
+
+# print(a+b)
+
+# type casting
+
+# a="2" #int nhi str hoga ye and error aa jaega
+# b=4.24
+
+# print(a+b)
+# so we use type casting
+
+# a=int("2")
+# b=4.24
+
+# print(type (a))
+# print(a+b)
+# ex-2
+# a=float("2")
+# b=4.24
+
+# print(type (a))
+# print(a+b)
+
+# ex3
+# a=3.14
+# a=str(a)
+
+# print(type(a))
+
+
+# all about inputs in python
+
+# input("enter your name:")#1
+
+# name= input("enter youur name")
+# print ("welcome", name)#2
+
+
+# name = input("enter your age:")
+# print("you entered", name)#3
+
+# input hamesha str me convert krta hai matlab type hamesha str aaega toh convert karane ke liye type casting use kro
+
+# val=input("enter some value:")
+# print(type(val),val)
+# yaha integer daalne pr bhi class str aaega so we use type casting
+
+# val=int(input("enter some value:"))
+# print(type(val),val)
+
+# val=float(input("enter some value:"))
+# print(type(val),val)
+
+# so corre ct way to use it is:
+
+# name=(input("enter your name:"))
+# age=(input("enter age:"))
+# marks=(input("enter marks:"))
+
+# print("welcome", name)
+# print("your age:", age)
+# print("marks:", marks)
+
+# name=(input("enter your name:"))
+# age=int(input("enter age:"))
+# marks=float(input("enter marks:"))
+
+# print("welcome", name)
+# print("your age:", age)
+# print("marks:", marks)
+
+
